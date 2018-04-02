fd8547699d8c733477303ec1f4077b258bf53495
William Bajzek <williambajzek@gmail.com>
2018-04-02 18:02:39 -0500

    add SimpleQuantizer and WaveFolder

---
+++ src/SimpleQuantizer.cpp
@@ -0,0 +1,72 @@
+#include "WTB_Basic.hpp"
+#include "dsp/digital.hpp"
+
+struct SimpleQuantizer : Module {
+	enum ParamIds {
+		PITCH_PARAM,
+		NUM_PARAMS
+	};
+	enum InputIds {
+    TRIGGER_INPUT,
+		PITCH_INPUT,
+		NUM_INPUTS
+	};
+	enum OutputIds {
+		QUANTIZED_OUTPUT,
+		NUM_OUTPUTS
+	};
+	enum LightIds {
+		BLINK_LIGHT,
+		NUM_LIGHTS
+	};
+
+	float phase = 0.0;
+  float quantized_pitch = 0.0;
+  SchmittTrigger trigger;
+
+	SimpleQuantizer() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+	void step() override;
+
+	// For more advanced Module features, read Rack's engine.hpp header file
+	// - toJson, fromJson: serialization of internal data
+	// - onSampleRateChange: event triggered by a change of sample rate
+	// - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
+};
+
+
+void SimpleQuantizer::step() {
+	float pitch = inputs[PITCH_INPUT].value;
+  if (trigger.process(inputs[TRIGGER_INPUT].value)) {
+    pitch = clamp(pitch, -4.0f, 4.0f);
+    pitch += 4.0f;
+    pitch *= 12.0/1.0;
+    pitch = (int)pitch;
+    pitch *= 1.0/12.0;
+    pitch -= 4.0f;
+    quantized_pitch = pitch;
+  }
+  outputs[QUANTIZED_OUTPUT].value = quantized_pitch;
+}
+
+
+struct SimpleQuantizerWidget : ModuleWidget {
+	SimpleQuantizerWidget(SimpleQuantizer *module) : ModuleWidget(module) {
+		setPanel(SVG::load(assetPlugin(plugin, "res/Quant.svg")));
+
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addInput(Port::create<PJ301MPort>(Vec(3, 110), Port::INPUT, module, SimpleQuantizer::TRIGGER_INPUT));
+
+		addInput(Port::create<PJ301MPort>(Vec(3, 205), Port::INPUT, module, SimpleQuantizer::PITCH_INPUT));
+
+		addOutput(Port::create<PJ301MPort>(Vec(3, 300), Port::OUTPUT, module, SimpleQuantizer::QUANTIZED_OUTPUT));
+	}
+};
+
+
+// Specify the Module and ModuleWidget subclass, human-readable
+// author name for categorization per plugin, module slug (should never
+// change), human-readable module name, and any number of tags
+// (found in `include/tags.hpp`) separated by commas.
+Model *modelSimpleQuantizer = Model::create<SimpleQuantizer, SimpleQuantizerWidget>("WTB", "SimpleQuantizer", "Simple Quantizer", UTILITY_TAG);
---
+++ src/WaveFolder.cpp
@@ -0,0 +1,65 @@
+#include "WTB_Basic.hpp"
+#include <cmath>
+
+struct WaveFolder : Module {
+	enum ParamIds {
+		AMOUNT_PARAM,
+		NUM_PARAMS
+	};
+	enum InputIds {
+    AMOUNT_INPUT,
+		SIGNAL_INPUT,
+		NUM_INPUTS
+	};
+	enum OutputIds {
+		SIGNAL_OUTPUT,
+		NUM_OUTPUTS
+	};
+	enum LightIds {
+		BLINK_LIGHT,
+		NUM_LIGHTS
+	};
+
+	WaveFolder() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+	void step() override;
+
+	// For more advanced Module features, read Rack's engine.hpp header file
+	// - toJson, fromJson: serialization of internal data
+	// - onSampleRateChange: event triggered by a change of sample rate
+	// - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
+};
+
+
+void WaveFolder::step() {
+  float input = inputs[SIGNAL_INPUT].value;
+  float amount = clamp(inputs[AMOUNT_INPUT].value, 0.0f, 5.0f) * params[AMOUNT_PARAM].value * 0.1f;
+  float output = input - cos(input * amount);
+  outputs[SIGNAL_OUTPUT].value = output;
+}
+
+
+struct WaveFolderWidget : ModuleWidget {
+	WaveFolderWidget(WaveFolder *module) : ModuleWidget(module) {
+		setPanel(SVG::load(assetPlugin(plugin, "res/WaveFolder.svg")));
+
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 4 * RACK_GRID_WIDTH, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 1 * RACK_GRID_WIDTH, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 4 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 1 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+    addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(13, 70), module, WaveFolder::AMOUNT_PARAM, 0.0, 5.0, 0.0));
+
+		addInput(Port::create<PJ301MPort>(Vec(18, 150), Port::INPUT, module, WaveFolder::AMOUNT_INPUT));
+		addInput(Port::create<PJ301MPort>(Vec(18, 225), Port::INPUT, module, WaveFolder::SIGNAL_INPUT));
+
+		addOutput(Port::create<PJ301MPort>(Vec(18, 300), Port::OUTPUT, module, WaveFolder::SIGNAL_OUTPUT));
+	}
+};
+
+
+// Specify the Module and ModuleWidget subclass, human-readable
+// author name for categorization per plugin, module slug (should never
+// change), human-readable module name, and any number of tags
+// (found in `include/tags.hpp`) separated by commas.
+Model *modelWaveFolder = Model::create<WaveFolder, WaveFolderWidget>("WTB", "WaveFolder", "Wave Folder", UTILITY_TAG);
+
